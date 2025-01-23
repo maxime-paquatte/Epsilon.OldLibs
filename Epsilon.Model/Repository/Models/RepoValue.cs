bc39973acecc9e817fe1f8952a6498639d3667f6
mpaquatte <maxime.paquatte@gmail.com>
2025-01-23 12:56:35 +0100

    Repo : Add QueryResult on Children
    (cherry picked from commit 8b4001f5ff9a3444c5680b581c932e3c6c467a8f)

---
+++ Epsilon.Model/Repository/Models/RepoValue.cs
@@ -0,0 +1,17 @@
+﻿namespace Epsilon.Model.Repository.Models;
+
+public class RepoValue
+{
+    public int CultureId { get; set; }
+    public string CultureKey { get; set; }
+    
+    public int RepoValueId { get; set; }
+    public int RepoId { get; set; }
+    public string Name { get; set; }
+    public string SystemKey { get; set; }
+    public int OldPlatformId { get; set; }
+    public int AlphaId { get; set; }
+    public string Value { get; set; }
+    public string Path { get; set; }
+    public string Content { get; set; }
+}
--- Epsilon.Model/Repository/Queries/Children.cs
+++ Epsilon.Model/Repository/Queries/Children.cs
@@ -2,6 +2,7 @@
 
 namespace Epsilon.Model.Repository.Queries
 {
+    [QueryResult(typeof(ChildrenResult))]
     public class Children : IQuery
     {
         public int RepoId { get; set; }
@@ -11,5 +12,10 @@ namespace Epsilon.Model.Repository.Queries
         public string Path { get; set; }
         
         public bool AllDepth { get; set; }
+    }
+    
+    public class ChildrenResult
+    {
+        public Models.RepoValue[] Values { get; set; }
     }
 }
